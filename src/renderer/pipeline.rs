d720d60c1b145b3bac9e1d9107c8894c17af0380
Shuflduf <shuflduf@gmail.com>
2026-06-04 17:01:17 -0600

    frog

--- src/renderer/pipeline.rs
+++ src/renderer/pipeline.rs
@@ -28,7 +28,7 @@ pub fn create_render_pipeline(
             entry_point: Some("fs_main"),
             targets: &[Some(wgpu::ColorTargetState {
                 format: config.format,
-                blend: Some(wgpu::BlendState::REPLACE),
+                blend: Some(wgpu::BlendState::ALPHA_BLENDING),
                 write_mask: wgpu::ColorWrites::ALL,
             })],
             compilation_options: wgpu::PipelineCompilationOptions::default(),
