f46ce0d99f6beb8521bd82cb17e1a00e966978df
Shuflduf <shuflduf@gmail.com>
2026-06-03 18:51:33 -0600

    fucking bullshit coordinate system

--- src/main.rs
+++ src/main.rs
@@ -32,19 +32,19 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.5, -0.5, 0.0],
+        position: [-0.5, 0.5, 0.0],
         tex_coords: [0.0, 0.0],
     },
     Vertex {
-        position: [-0.5, 0.5, 0.0],
+        position: [-0.5, -0.5, 0.0],
         tex_coords: [0.0, 1.0],
     },
     Vertex {
-        position: [0.5, -0.5, 0.0],
+        position: [0.5, 0.5, 0.0],
         tex_coords: [1.0, 0.0],
     },
     Vertex {
-        position: [0.5, 0.5, 0.0],
+        position: [0.5, -0.5, 0.0],
         tex_coords: [1.0, 1.0],
     },
 ];
