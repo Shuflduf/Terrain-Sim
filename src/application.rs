66ca66714ed4d74b4f93474a5fd3a3c159541ccb
Shuflduf <shuflduf@gmail.com>
2026-06-03 19:13:33 -0600

    starting file org

---
+++ src/application.rs
@@ -0,0 +1,69 @@
+use std::sync::Arc;
+
+use crate::state::State;
+use winit::{
+    application::ApplicationHandler,
+    event::*,
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
+
+pub struct Application {
+    state: Option<State>,
+}
+
+impl Application {
+    pub fn new() -> Self {
+        Self { state: None }
+    }
+}
+
+impl ApplicationHandler<State> for Application {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes();
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+        self.state = Some(pollster::block_on(State::new(window)).unwrap());
+    }
+
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, event: State) {
+        self.state = Some(event)
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.update();
+                match state.render() {
+                    Ok(_) => {}
+                    Err(e) => {
+                        log::error!("{e}");
+                        event_loop.exit();
+                    }
+                }
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => state.handle_key(event_loop, code, key_state.is_pressed()),
+            _ => {}
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use winit::event_loop::EventLoop;
+
+use crate::application::Application;
+
+mod application;
+mod mesh;
+mod state;
+
+pub fn run() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut app = Application::new();
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
+}
